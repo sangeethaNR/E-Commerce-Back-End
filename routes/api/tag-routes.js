f33627f81aa0962cc274d3f8271546880acb8c89
sangeethaNR <sangeethajadhav123@gmail.com>
2021-09-05 06:50:57 -0700

    tag route CURD added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,92 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+ 
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, as:"tag_product",
+      
+      }],
+    }).then(resTagData => res.json(resTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product,as:"tag_product",
+     }],
+    }).then(resTagData => res.json(resTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+  //   if (!tagData) {
+  //     res.status(404).json({ message: 'No library card found with that id!' });
+  //     return;
+  //   }
+
+  //   res.status(200).json(tagData);
+  // }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+ 
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    })
+    .then(resTagData => res.json(resTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+ 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+try{
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+ .then(function(rowsUpdated) {
+   res.json(rowsUpdated)
+ })
+}
+catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
